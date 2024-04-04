a3fb104fe208ed709840c9da0f940eacf28f8bbe
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-04-04 20:17:48 +0900

    🎉 Project init

---
+++ academic-information-system/src/main/java/hansung/ac/kr/academicinformationsystem/AcademicInformationSystemApplication.java
@@ -0,0 +1,13 @@
+package hansung.ac.kr.academicinformationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicInformationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicInformationSystemApplication.class, args);
+	}
+
+}
---
+++ academic-information-system/src/test/java/hansung/ac/kr/academicinformationsystem/AcademicInformationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package hansung.ac.kr.academicinformationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicInformationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
