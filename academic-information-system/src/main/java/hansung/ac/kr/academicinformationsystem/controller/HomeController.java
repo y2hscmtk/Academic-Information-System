b3040fec6090d35879fa4abada1b8071f01266d9
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-04-04 22:38:43 +0900

    add : HomeController, home.html

---
+++ academic-information-system/src/main/java/hansung/ac/kr/academicinformationsystem/controller/HomeController.java
@@ -0,0 +1,14 @@
+package hansung.ac.kr.academicinformationsystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    // 메인 화면
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+}
