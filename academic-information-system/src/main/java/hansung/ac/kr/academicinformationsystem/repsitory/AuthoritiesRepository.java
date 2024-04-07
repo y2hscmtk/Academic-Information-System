bd8af02b46152a02e79ab97e7f1b5dd8b6451dab
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-04-07 19:48:55 +0900

    ✨ 모든 Course, 특정 Course 정보 조회

--- academic-information-system/src/main/java/hansung/ac/kr/academicinformationsystem/repsitory/AuthoritiesRepository.java
+++ academic-information-system/src/main/java/hansung/ac/kr/academicinformationsystem/repsitory/AuthoritiesRepository.java
@@ -1,10 +1,10 @@
 package hansung.ac.kr.academicinformationsystem.repsitory;
 
-import hansung.ac.kr.academicinformationsystem.domain.Authorities;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-public interface AuthoritiesRepository extends JpaRepository<Authorities,String> {
+@Transactional
+public interface AuthoritiesRepository {
 
 }
--- academic-information-system/src/main/java/hansung/ac/kr/academicinformationsystem/repsitory/CourseRepository.java
+++ academic-information-system/src/main/java/hansung/ac/kr/academicinformationsystem/repsitory/CourseRepository.java
@@ -1,10 +1,31 @@
 package hansung.ac.kr.academicinformationsystem.repsitory;
 
 import hansung.ac.kr.academicinformationsystem.domain.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface CourseRepository extends JpaRepository<Course,String> {
+@RequiredArgsConstructor
+public class CourseRepository{
+
+    private final EntityManager em;
+
+    // 모든 Course정보 조회
+    public List<Course> getAllCourses(){
+        // 모든 Course정보 얻기
+        return em.createQuery("select c from Course c", Course.class)
+                .getResultList(); // List 형식으로 데이터 얻기
+    }
+
+    // 특정 학년 & 학기 정보에 해당하는 Course 정보 조회
+    public List<Course> getSpecificCourses(int year, int semester){
+        return em.createQuery(
+                "select c from Course c where c.year = :year and c.semester = :semester",Course.class)
+                .getResultList();
+    }
+
 
 }
