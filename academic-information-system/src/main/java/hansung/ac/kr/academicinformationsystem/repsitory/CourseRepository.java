aa802f5ac6dcbb8565301bcd1c0c1f3113d23a99
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-04-07 19:04:21 +0900

    ✨ Repository 인터페이스 추가

---
+++ academic-information-system/src/main/java/hansung/ac/kr/academicinformationsystem/repsitory/CourseRepository.java
@@ -0,0 +1,10 @@
+package hansung.ac.kr.academicinformationsystem.repsitory;
+
+import hansung.ac.kr.academicinformationsystem.domain.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course,String> {
+
+}
---
+++ academic-information-system/src/main/java/hansung/ac/kr/academicinformationsystem/repsitory/UserRepository.java
@@ -0,0 +1,9 @@
+package hansung.ac.kr.academicinformationsystem.repsitory;
+
+import hansung.ac.kr.academicinformationsystem.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,String> {
+}
