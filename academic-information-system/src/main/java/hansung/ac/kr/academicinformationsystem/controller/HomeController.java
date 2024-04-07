b8593e375d4e4094233f920944169ab9e402b2bd
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-04-07 18:24:31 +0900

    ✨ EnrolmentService, GradeService 프레임 추가

--- academic-information-system/src/main/java/hansung/ac/kr/academicinformationsystem/controller/HomeController.java
+++ academic-information-system/src/main/java/hansung/ac/kr/academicinformationsystem/controller/HomeController.java
@@ -1,20 +1,14 @@
 package hansung.ac.kr.academicinformationsystem.controller;
 
-import hansung.ac.kr.academicinformationsystem.domain.Course;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.ArrayList;
-
-// 2) 학사 정보 시스템에 대한 홈페이지를 작성한다. 학사 정보 접근 메뉴는
-// ① 학년별 이수 학점 조회,
-// ② 수강 신청하기,
-// ③ 수강 신청 조회가 있다. 이 때 홈페이지는 누구나 접근 가능하며,학사 정보 접근은 반드시 로그인을 수행한 후, 접근할 수 있다.
 @Controller
 public class HomeController {
 
     // 메인 화면 => 누구나 접근 가능
+    // <제공 화면>
+    // 수강 신청 / 수강 신청 조회 / 이수 학점 조회
     @GetMapping("/")
     public String home(){
         return "home";
---
+++ academic-information-system/src/main/java/hansung/ac/kr/academicinformationsystem/service/GradeService.java
@@ -0,0 +1,9 @@
+package hansung.ac.kr.academicinformationsystem.service;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class GradeService {
+    // 1. 학기별 이수한 모든 수업 데이터 얻어오기
+    // 2. 수업데이터 바탕으로 총계 계산(가장 마지막 행에 취득학점 총계 표시)
+}
